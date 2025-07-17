546337d1f0befa6824327e6880b7119abf59690d
Jason Zheng <jasonzheng0814@gmail.com>
2025-07-17 14:05:27 -0400

    initial connect app to MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "../routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,4 +12,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
+  connectDB();
 });
