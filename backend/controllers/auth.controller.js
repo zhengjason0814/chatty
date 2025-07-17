63a8370204aab461b4e7613e03ad55d2221713c7
Jason Zheng <jasonzheng0814@gmail.com>
2025-07-17 10:36:43 -0400

    set up best practice for routes in auth

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res) {
+  res.send("Signup Route");
+}
+
+export async function login(req, res) {
+  res.send("Login Route");
+}
+
+export function logout(req, res) {
+  res.send("Logout Route");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "../routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/api/auth/signup", (req, res) => {
-  res.send("Sign-Up Route");
-});
-
-app.get("/api/auth/login", (req, res) => {
-  res.send("Login Route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  res.send("Sign-Out Route");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
