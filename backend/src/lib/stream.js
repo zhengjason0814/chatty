a634fa99747d2ec879b434e50d948bf9de350a8b
Jason Zheng <jasonzheng0814@gmail.com>
2025-07-18 14:40:37 -0400

    add getStreamToken controller method, finish initial backend

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -19,4 +19,11 @@ export const upsertStreamUser = async (userData) => {
   }
 };
 
-export const generateStreamToken = (userId) => {};
+export const generateStreamToken = (userId) => {
+  try {
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error generating Stream Token:", error);
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/app/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
