553adcbf9b9b24fc0c3d359f8f96139fc7467308
Jason Zheng <jasonzheng0814@gmail.com>
2025-07-22 19:17:51 -0400

    fix typo error in chat controller method

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -4,7 +4,7 @@ export async function getStreamToken(req, res) {
   try {
     const token = generateStreamToken(req.user.id);
 
-    res.status(200).json(token);
+    res.status(200).json({ token });
   } catch (error) {
     console.log("Error in getStreamToken controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
